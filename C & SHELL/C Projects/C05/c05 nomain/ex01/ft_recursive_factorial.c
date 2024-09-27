f4b550ff3203909a9c8972ace3f3265bf91598fe
Muhammed Kallo <massagekelo@gmail.com>
2024-09-27 19:11:08 +0300

    ALL FILES ADDED 27/09/2024

---
+++ C & SHELL/C Projects/C05/c05 nomain/ex01/ft_recursive_factorial.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkallo <mkallo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/01 18:55:39 by mkallo            #+#    #+#             */
+/*   Updated: 2024/09/03 14:54:09 by mkallo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	int	result;
+
+	result = 1;
+	if (nb < 0)
+		return (0);
+	if (nb > 0)
+	{
+		result *= nb * ft_recursive_factorial(nb - 1);
+	}
+	return (result);
+}
